2dcba507db45da7f55b813665d2c61c5286664f5
Your Name <ilyos.sultanov2020@gmail.com>
2023-06-05 17:26:47 +0500

    converted js to typescript

---
+++ typescript/src/index.ts
@@ -0,0 +1,100 @@
+let input: any = document.getElementById("input");
+let addBtn: any = document.getElementById("addBtn");
+let emptyMessage: any = document.getElementById("emptyMessage");
+let tasksSection = document.getElementById("tasks");
+let tasksList: any = document.getElementById("tasksList");
+let tasks: any[] = [];
+
+addBtn.addEventListener("click", (e: any) => {
+  e.preventDefault();
+  addTodo();
+  if (e.keyCode == 15) addTodo();
+});
+
+function addTodo() {
+  if (input.value == "") {
+    return;
+  } else {
+    emptyMessage.style.display = "none";
+    let data = {
+      id: tasks.length,
+      name: input.value.toUpperCase().trim(),
+    };
+    tasks.push(data);
+    input.value = "";
+    loadTasks(tasks);
+  }
+}
+
+function loadTasks(tasks: any) {
+  tasksList.innerHTML = " ";
+  tasks.forEach((task: any) => {
+    const todoLI = document.createElement("li");
+    todoLI.className = "task";
+    todoLI.id = task.id;
+    todoLI.style.animation = "fadeIn 2s";
+
+    const todoP = document.createElement("p");
+    todoP.className = "task-name";
+    todoP.innerText = task.name;
+
+    const buttons = document.createElement("div");
+    buttons.className = "task__buttons";
+
+    const editBtn = document.createElement("button");
+    editBtn.className = "edit-btn";
+    editBtn.innerText = "Edit";
+    editBtn.addEventListener("click", () => {
+      todoP.innerHTML = "";
+      let editInput = document.createElement("input");
+      editInput.type = "text";
+      editInput.className = "edit-input";
+      let saveBtn = document.createElement("button");
+      saveBtn.innerText = "Save";
+      saveBtn.className = "save-btn";
+
+      todoP.appendChild(editInput);
+      todoP.appendChild(saveBtn);
+      editInput.value = task.name;
+
+      saveBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        task.name = editInput.value;
+        editInput.remove();
+        todoP.innerText = task.name;
+      });
+    });
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "delete-btn";
+    deleteBtn.innerText = "Delete";
+    deleteBtn.addEventListener("click", () => {
+      todoLI.style.animation = "fadeOut 2s";
+      delete tasks[task.id];
+      tasks = tasks.filter((task: any) => {
+        if (task !== undefined) {
+          return task;
+        }
+      });
+
+      console.log(tasks);
+
+      setTimeout(() => {
+        if (tasks.length == 0) {
+          todoLI.remove();
+          emptyMessage.style.display = "flex";
+        } else {
+          todoLI.remove();
+
+          return;
+        }
+      }, 500);
+    });
+
+    buttons.appendChild(editBtn);
+    buttons.appendChild(deleteBtn);
+    todoLI.appendChild(todoP);
+    todoLI.appendChild(buttons);
+    tasksList.appendChild(todoLI);
+  });
+}
